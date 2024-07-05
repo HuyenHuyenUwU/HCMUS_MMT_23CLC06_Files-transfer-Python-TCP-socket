05da8d9a678e077b1c2f1a34049dd963c0a1ba02
PhamNamHuyen-23127057 <pnhuyen23@clc.fitus.edu.vn>
2024-07-05 22:11:45 +0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,48 +1,48 @@
-# Logic của việc mà mình truyền file
-# Sender
-# Reciever
-# Upload: Data: client-> serever => client: gửi, server: nhận
-# Download: data đi từ server->client => client: nhận, server: gửi
+# # Logic của việc mà mình truyền file
+# # Sender
+# # Reciever
+# # Upload: Data: client-> serever => client: gửi, server: nhận
+# # Download: data đi từ server->client => client: nhận, server: gửi
 
-# Sender: 3 bước
-# 1. truy cập vào browser -> select folder
-# 2. SPLIT file thành N segment
-# 3. Tạo N thread tương ứng -> SEND 
+# # Sender: 3 bước
+# # 1. truy cập vào browser -> select folder
+# # 2. SPLIT file thành N segment
+# # 3. Tạo N thread tương ứng -> SEND 
 
-# Reciever: 3 bước
-# 1. Nhận N segments 
-# 2. MERGE các file đã nhận -> save vào 2 cái folder 
-# 3. (optional) xóa các file bị thừa
+# # Reciever: 3 bước
+# # 1. Nhận N segments 
+# # 2. MERGE các file đã nhận -> save vào 2 cái folder 
+# # 3. (optional) xóa các file bị thừa
 
-#libraries
-import os
-import socket
-import tkinter #message box, tk, filedialog, simpledialog
-import threading
+# #libraries
+# import os
+# import socket
+# import tkinter #message box, tk, filedialog, simpledialog
+# import threading
 
-#constant
-HOST = 'localhost' #127.0.0.1
-PORT = 9999
-CHUNK_SIZE = 1024 # 1kb
-UPLOAD_FOLDER = 'Client_data'
+# #constant
+# HOST = 'localhost' #127.0.0.1
+# PORT = 9999
+# CHUNK_SIZE = 1024 # 1kb
+# UPLOAD_FOLDER = 'Client_data'
 
-#function
-select_file_to_upload() #Ngan
+# #function
+# select_file_to_upload() #Ngan
 
-# UPLOAD
-upload_file(file_path) # 1. split file thanh cac chunk nho -> chay mot vong for de gui tung chunk mot (nho kiem tra tat ca chunk da qua het chua)
-split_file(file_path, chunk_size) # tach file lon -> N files nho #Ngan
-# chunks[] -> code -> return chunks
+# # UPLOAD
+# upload_file(file_path) # 1. split file thanh cac chunk nho -> chay mot vong for de gui tung chunk mot (nho kiem tra tat ca chunk da qua het chua)
+# split_file(file_path, chunk_size) # tach file lon -> N files nho #Ngan
+# # chunks[] -> code -> return chunks
 
-#DOWNLOAD
-recieve_file(filename, num_chunk)
-merge_chunks(chunks, outputfile) # tao mot file output moi -> chay mot vong for -> write tung chunk nho vao(nho kiem tra tat ca chunk da qua het chua)
+# #DOWNLOAD
+# recieve_file(filename, num_chunk)
+# merge_chunks(chunks, outputfile) # tao mot file output moi -> chay mot vong for -> write tung chunk nho vao(nho kiem tra tat ca chunk da qua het chua)
 
-# main()
-# tao socket
-# tao connection -> signal connect with client nao
-# chen gui elements (button, thanh tien trinh, chu thich...)
+# # main()
+# # tao socket
+# # tao connection -> signal connect with client nao
+# # chen gui elements (button, thanh tien trinh, chu thich...)
 
 
-#main entry point
+# #main entry point
 
--- server.py
+++ server.py
@@ -1,62 +1,62 @@
-# Logic của việc mà mình truyền file
-# Sender
-# Reciever
-# Upload: Data: client-> serever => client: gửi, server: nhận
-# Download: data đi từ server->client => client: nhận, server: gửi
+# # Logic của việc mà mình truyền file
+# # Sender
+# # Reciever
+# # Upload: Data: client-> serever => client: gửi, server: nhận
+# # Download: data đi từ server->client => client: nhận, server: gửi
 
-# Sender: 3 bước
-# 1. truy cập vào browser -> select folder
-# 2. SPLIT file thành N segment
-# 3. Tạo N thread tương ứng -> SEND 
+# # Sender: 3 bước
+# # 1. truy cập vào browser -> select folder
+# # 2. SPLIT file thành N segment
+# # 3. Tạo N thread tương ứng -> SEND 
 
-# Reciever: 3 bước
-# 1. Nhận N segments 
-# 2. MERGE các file đã nhận -> save vào 2 cái folder 
-# 3. (optional) xóa các file bị thừa
-#libraries
-import os
-import socket
-import tkinter #message box, tk, filedialog, simpledialog
-import threading
+# # Reciever: 3 bước
+# # 1. Nhận N segments 
+# # 2. MERGE các file đã nhận -> save vào 2 cái folder 
+# # 3. (optional) xóa các file bị thừa
+# #libraries
+# import os
+# import socket
+# import tkinter #message box, tk, filedialog, simpledialog
+# import threading
 
-#constant
-HOST = 'localhost' #127.0.0.1
-PORT = 9999
-CHUNK_SIZE = 1024 # 1kb
-UPLOAD_FILE  = 'Server_data'
+# #constant
+# HOST = 'localhost' #127.0.0.1
+# PORT = 9999
+# CHUNK_SIZE = 1024 # 1kb
+# UPLOAD_FILE  = 'Server_data'
 
-#function
-handle_client(conn, addr) # xu li yeu cau tu client
-# try:
-# request type: command tu client
-# == 'upload' -> handle_upload
-# == 'download' -> handle download
-# except: invalid request (error)
-# finally: conn.close()
+# #function
+# handle_client(conn, addr) # xu li yeu cau tu client
+# # try:
+# # request type: command tu client
+# # == 'upload' -> handle_upload
+# # == 'download' -> handle download
+# # except: invalid request (error)
+# # finally: conn.close()
 
-handle_upload()
-# 1. Nhận N segments 
-# 2. MERGE các file đã nhận -> save vào 2 cái folder 
-merge_chunks(chunks, outputfile)
-# 3. (optional) xóa các file bị thừa
+# handle_upload()
+# # 1. Nhận N segments 
+# # 2. MERGE các file đã nhận -> save vào 2 cái folder 
+# merge_chunks(chunks, outputfile)
+# # 3. (optional) xóa các file bị thừa
 
-handle_download()
-# Sender: 3 bước
-# 1. truy cập vào browser -> select folder
-# 2. SPLIT file thành N segment
-split_file(file_path, chunk_size) 
-select_file_to_download()
+# handle_download()
+# # Sender: 3 bước
+# # 1. truy cập vào browser -> select folder
+# # 2. SPLIT file thành N segment
+# split_file(file_path, chunk_size) 
+# select_file_to_download()
 
-# 3. Tạo N thread tương ứng -> SEND 
+# # 3. Tạo N thread tương ứng -> SEND 
 
-start_server()
-# tao socket
-# tao connection -> signal connect with client nao
-# chen gui elements (button, thanh tien trinh, chu thich...)
-# hoac print nhung cai tin hieu cmd
-#ex:
-# connecting to client 1...
-# recieve filename form client1...
+# start_server()
+# # tao socket
+# # tao connection -> signal connect with client nao
+# # chen gui elements (button, thanh tien trinh, chu thich...)
+# # hoac print nhung cai tin hieu cmd
+# #ex:
+# # connecting to client 1...
+# # recieve filename form client1...
 
 
 
